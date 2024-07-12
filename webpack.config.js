35e5d507ac0280316757ffded865775aa875ed9b
Simon Jern <simonnjern@gmail.com>
2024-07-12 21:35:58 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
-export const entry = ".src/g-calendar-fetcher.js";
-export const output = {
-  filename: "g-calendar-fetcher.min.js",
-  path: resolve(__dirname, "dist"),
-  library: "GCalendarFetcher",
-  libraryTarget: "umd",
-  globalObject: "this",
-};
-export const mode = "production";
-export const optimization = {
-  minimize: true,
-  minimizer: [
-    new TerserPlugin({
-      terserOptions: {
-        output: {
-          comments: false,
+module.exports = {
+  entry: ".src/g-calendar-fetcher.js",
+  output: {
+    filename: "g-calendar-fetcher.min.js",
+    path: resolve(__dirname, "dist"),
+    library: "GCalendarFetcher",
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+          compress: {
+            drop_console: true,
+          },
+          mangle: {
+            reserved: [
+              "FetchError",
+              "ParseError",
+              "CreationError",
+              "GCalendarFetcher",
+            ],
+          },
         },
-        compress: {
-          drop_console: true,
-        },
-        mangle: {
-          reserved: [
-            "FetchError",
-            "ParseError",
-            "CreationError",
-            "GCalendarFetcher",
-          ],
-        },
-      },
-    }),
-  ],
+      }),
+    ],
+  },
 };
